2ec1f76a44c7ee3cd31c07f4f491b8b777103fc9
joshuali925 <joshuali925@gmail.com>
2017-11-15 23:22:17 -0500

    Update processcsv.h

--- 2017Fall/project2/processcsv.h
+++ 2017Fall/project2/processcsv.h
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <pthread.h>
 char isnum(char *s);
 void *processcsv(void *oripath);
-void *multisort(void *oripath);
-void appenddir(char *npath, char *path, char *path2);
-char ***readdata(FILE * fp, int collen, int filecounter, int maxlen);
-int twowaymerge(char ****table, int file1, int file2, int linelen[], int type, int tosort);
-char ***kwaymerge(char ****table, int filenum, int linelen[], int type, int tosort);
+void *traversedir(void *oripath);
+char ***readdata(FILE * fp, int filecounter);
+void *twowaymerge(void *context);
+void kwaymerge();
 void outputcolumn(char **column, int collen);
+void outputfile();
+void getcolinfo();
 
 
 #include "mergesort.c"
 void mergesort(char ***s, int low, int high, char type, int tosort);
+
+
