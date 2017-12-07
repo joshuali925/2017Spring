2576b98a863e2e59a300130e7f3480ed3d529d89
joshuali925 <joshuali925@gmail.com>
2017-12-07 18:00:23 -0500

    Create sorter_client.h

---
+++ 2017Fall/project3/sorter_client.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/sendfile.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <errno.h>
+
+char isnum(char *s);
+void *transfer(void *rawpath);
+void *traversedir(void *oripath);
+void getcolinfo();
+void sendcolinfo();
+void mergerequest();
+int createsocket();
+
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
